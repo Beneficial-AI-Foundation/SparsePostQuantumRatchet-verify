ad12fdc5432ae2b5838c9cfe5e31998b344c2a14
hoangtruong <truonghoangle@gmail.com>
2026-07-17 10:19:44 +0200

    edited

--- Spqr/Specs/Encoding/Polynomial/COMPLETE_POINTS_POLYS_36.lean
+++ Spqr/Specs/Encoding/Polynomial/COMPLETE_POINTS_POLYS_36.lean
@@ -30,31 +30,8 @@ theorem COMPLETE_POINTS_POLYS_36_spec :
         listToGF216Poly (result.val[j]!).coefficients.val =
           scaledLagrangeBasis 36#usize j ⦄ := by
   unfold COMPLETE_POINTS_POLYS_36
-  step*
-  have h_eq : result = completePoints 36#usize := by
-    simp only [global_simps]
-    apply Subtype.ext
-    apply List.ext_getElem (by simp)
-    intro n h1' h2'
-    obtain ⟨hx, hy⟩ := result_post1 n (by grind)
-    simp only [Array.getElem!_Nat_eq, List.getElem!_eq_getElem?_getD,
-      List.getElem?_eq_getElem h1', Option.getD_some] at hx hy
-    simp only [List.getElem_map, List.getElem_finRange] at h2' ⊢
-    apply pt_ext
-    · apply gf16_ext
-      apply UScalar.eq_of_val_eq
-      trans n
-      · exact hx
-      · change n = (⟨BitVec.ofNat 16 n⟩ : UScalar .U16).bv.toNat
-        simp [BitVec.toNat_ofNat]
-        grind
-    · exact hy.trans (gf16_ext GF16.ONE_value)
-  have h := result_post2 _ result_post3 (by grind) (by grind)
-  rw [h_eq] at h
-  simp only [global_simps] at h ⊢
-  have h36 : (↑(36#usize) : Nat) = 36 := by scalar_tac
-  simp only [h36] at h ⊢
-  simp only [List.getElem!_eq_getElem?_getD] at ⊢
-  grind
+  have h36 : (36#usize).val = 36 := by scalar_tac
+  simpa [h36] using
+    lagrange_polys_for_complete_points_scaled_spec 36#usize (by scalar_tac) (by scalar_tac)
 
 end spqr.encoding.polynomial
