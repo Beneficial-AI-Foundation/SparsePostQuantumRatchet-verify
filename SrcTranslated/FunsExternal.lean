517eb67614fcac5a07e156bf27667b415388073b
Oliver Butterley <51876429+oliver-butterley@users.noreply.github.com>
2026-07-18 08:14:06 +0100

    Enable a header-style linter and correct problems flagged by the linter (#319)
    Mathlib has a header linter (`linter.style.header`) which is now active
    by default. However it hardcodes the required copyright line and so is
    incompatible with this project's LICENSE-APACHE file. There is a [PR to
    mathlib](https://github.com/leanprover-community/mathlib4/pull/41876)
    which adds a config option for that linter.
    
    This PR adds a copy of that modified linter locally so that we can use
    it immediately (+469 lines from this file). This local version
    (`HeaderLinter.lean`) will be deleted once the mathlib PR lands.
    
    The mathlib linter requires the `(c)` in the copyright notice. This PR
    updates everything we have to this format so we conform with what is
    common even though it's legally superfluous.

--- SrcTranslated/FunsExternal.lean
+++ SrcTranslated/FunsExternal.lean
@@ -1,12 +1,15 @@
 import Aeneas
 import SrcTranslated.Types
-open Aeneas Aeneas.Std Result ControlFlow Error
+
+set_option linter.style.headerAlt false
 set_option linter.dupNamespace false
 set_option linter.hashCommand false
 set_option linter.unusedVariables false
 set_option linter.style.longLine false
 set_option linter.style.setOption false
 set_option linter.style.whitespace false
+
+open Aeneas Aeneas.Std Result ControlFlow Error
 
 /- You can set the `maxHeartbeats` value with the `-max-heartbeats` CLI option -/
 set_option maxHeartbeats 1000000
