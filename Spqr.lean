e928ebf30d25ef7976afb1ac87af05db7aef1804
Hoang Le Truong <40373735+truonghoangle@users.noreply.github.com>
2026-08-12 16:05:16 +0200

    Specify and verify : `spqr::{spqr::SecretOutput}::has_secret` in `lib.rs` (#445)
    This PR specifies and verifies :
    `spqr::{spqr::SecretOutput}::has_secret` in `lib.rs`
    
    closes #421

--- Spqr.lean
+++ Spqr.lean
@@ -157,7 +157,9 @@ import Spqr.Specs.Lib.EmptyState
 import Spqr.Specs.Lib.Error.From
 import Spqr.Specs.Lib.SecretOutput.Eq
 import Spqr.Specs.Lib.SecretOutput.Fmt
+import Spqr.Specs.Lib.SecretOutput.HasSecret
 import Spqr.Specs.Lib.SecretOutput.RecvSecret
+import Spqr.Specs.Lib.SecretOutput.SendSecret
 import Spqr.Specs.Serialize.Error.Clone
 import Spqr.Specs.Serialize.Error.Eq
 import Spqr.Specs.Serialize.Error.From
