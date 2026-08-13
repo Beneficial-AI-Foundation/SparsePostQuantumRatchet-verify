fb028bd906a87a3f5e7f549cf8d8efd8b1203e43
hoangtruong <truonghoangle@gmail.com>
2026-08-13 12:13:12 +0200

    saved

--- Spqr.lean
+++ Spqr.lean
@@ -161,6 +161,7 @@ import Spqr.Specs.Lib.SecretOutput.HasSecret
 import Spqr.Specs.Lib.SecretOutput.RecvSecret
 import Spqr.Specs.Lib.SecretOutput.Secret
 import Spqr.Specs.Lib.SecretOutput.SendSecret
+import Spqr.Specs.Lib.Version.DISABLED
 import Spqr.Specs.Lib.Version.U8.From
 import Spqr.Specs.Serialize.Error.Clone
 import Spqr.Specs.Serialize.Error.Eq
