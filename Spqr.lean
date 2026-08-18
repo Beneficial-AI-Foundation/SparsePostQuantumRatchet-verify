f8094f395b8cc9042fdd93c22e5bcdb6881e747b
Hoang Le Truong <40373735+truonghoangle@users.noreply.github.com>
2026-08-18 10:19:22 +0200

    Verify and specify `spqr::chain::ChainParams::clone` in `chain.rs` (#465)
    This PR verifies and specifies `spqr::chain::ChainParams::clone` in
    `chain.rs`
    
    closes #454

--- Spqr.lean
+++ Spqr.lean
@@ -60,6 +60,7 @@ import Spqr.Specs.Authenticator.Authenticator.MacCt
 import Spqr.Specs.Authenticator.Authenticator.MacHdr
 import Spqr.Specs.Authenticator.Serialize.Authenticator.FromPb
 import Spqr.Specs.Authenticator.Serialize.Authenticator.IntoPb
+import Spqr.Specs.Chain.ChainParams.Clone
 import Spqr.Specs.Encoding.Decoder.AddChunk
 import Spqr.Specs.Encoding.Decoder.DecodedMessage
 import Spqr.Specs.Encoding.Decoder.New
