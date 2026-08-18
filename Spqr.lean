32d19b8e2d8050e782c95d99c0b1e30bac3dbddd
hoangtruong <truonghoangle@gmail.com>
2026-08-18 21:33:37 +0200

    saved

--- Spqr.lean
+++ Spqr.lean
@@ -61,6 +61,7 @@ import Spqr.Specs.Authenticator.Authenticator.MacHdr
 import Spqr.Specs.Authenticator.Serialize.Authenticator.FromPb
 import Spqr.Specs.Authenticator.Serialize.Authenticator.IntoPb
 import Spqr.Specs.Chain.ChainParams.Clone
+import Spqr.Specs.Chain.ChainParams.Default
 import Spqr.Specs.Encoding.Decoder.AddChunk
 import Spqr.Specs.Encoding.Decoder.DecodedMessage
 import Spqr.Specs.Encoding.Decoder.New
