20867b8bb375af76ba366659457b88bd40fb9960
Oliver Butterley <oliver.butterley@gmail.com>
2026-08-18 20:56:01 +0200

    Specify and verify : `spqr::authenticator::Authenticator::verify_hdr` in `authenticator.rs`

--- Spqr.lean
+++ Spqr.lean
@@ -59,6 +59,7 @@ import Spqr.Specs.Authenticator.Authenticator.MACSIZE
 import Spqr.Specs.Authenticator.Authenticator.MacCt
 import Spqr.Specs.Authenticator.Authenticator.MacHdr
 import Spqr.Specs.Authenticator.Authenticator.VerifyCt
+import Spqr.Specs.Authenticator.Authenticator.VerifyHdr
 import Spqr.Specs.Authenticator.Serialize.Authenticator.FromPb
 import Spqr.Specs.Authenticator.Serialize.Authenticator.IntoPb
 import Spqr.Specs.Chain.ChainParams.Clone
