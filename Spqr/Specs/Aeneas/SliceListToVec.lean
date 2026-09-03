55fc20e008cfc7fa95f6eb635ab24354177344ae
hoangtruong <truonghoangle@gmail.com>
2026-09-03 07:03:30 +0200

    saved

--- Spqr/Specs/Aeneas/SliceListToVec.lean
+++ Spqr/Specs/Aeneas/SliceListToVec.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 The Beneficial AI Foundation. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE-APACHE.
-Authors: Cline
+Authors: Hoang Le Truong
 -/
 import SrcTranslated.Funs
 
