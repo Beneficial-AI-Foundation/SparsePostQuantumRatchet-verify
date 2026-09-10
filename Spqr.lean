83c1d361b2261501d0dd1739e786fa6ca8becb1a
Hoang Le Truong <40373735+truonghoangle@users.noreply.github.com>
2026-09-10 08:52:09 +0200

    specify and verify `spqr::chain::{spqr::chain::ChainEpochDirection}::clear_next` in `chain.rs` (#536)
    This PR specifies and verifies
    `spqr::chain::{spqr::chain::ChainEpochDirection}::clear_next` in
    `chain.rs`
    
    closes #502

--- Spqr.lean
+++ Spqr.lean
@@ -73,6 +73,7 @@ import Spqr.Specs.Authenticator.Authenticator.VerifyCt
 import Spqr.Specs.Authenticator.Authenticator.VerifyHdr
 import Spqr.Specs.Authenticator.Serialize.Authenticator.FromPb
 import Spqr.Specs.Authenticator.Serialize.Authenticator.IntoPb
+import Spqr.Specs.Chain.ChainEpochDirection.ClearNext
 import Spqr.Specs.Chain.ChainEpochDirection.FromPb
 import Spqr.Specs.Chain.ChainEpochDirection.IntoPb
 import Spqr.Specs.Chain.ChainEpochDirection.Key
